c7a48618f568d0383ae091ed86b3607a0a0c3f72
Omar Ragab <oragab1518@gmail.com>
2022-02-11 23:28:20 -0700

    test2

--- server/server.js
+++ server/server.js
@@ -78,7 +78,7 @@ app.use(express.static("client/build"));
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
 console.log(__dirname);
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 
 /* -------------------------------- Socket.IO ------------------------------- */
